a7be787aa1574d241aeab2043898f7eddb240581
khaled <kzack-dev@hotmail.com>
2020-08-20 14:51:39 +0300

    remove old comments

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,6 @@ import { reducer } from './validators';
 import { isPlainObject } from './utils';
 
 export default function createModel<T extends EmptyObject>(model: Model) {
-  // satisfiy model object requirements
   if (!isPlainObject(model)) {
     throw new Error('model should be a valid object');
   }
@@ -12,7 +11,6 @@ export default function createModel<T extends EmptyObject>(model: Model) {
 
   return Object.freeze({
     validate(data: T) {
-      // satisfy data shape requirements
       if (!isPlainObject(data)) {
         throw new Error('data should be a valid object');
       }
@@ -23,12 +21,13 @@ export default function createModel<T extends EmptyObject>(model: Model) {
         const key = modelKeys[i];
         const validator = model[key];
         const valueToValidate = data[key];
-        // convert short hand properties to its uniform shape for easier validation handling
+
         if (!isPlainObject(validator) || !validator) {
           throw new Error(`Invalid validator "${key}"`);
         }
-        // safe to assume that validation is an object that satisfy validation types
+
         const err = reducer(key, validator, valueToValidate);
+
         if (err) errors.push(err);
       }
 
