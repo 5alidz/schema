b29734b955a4caae13fe5a79de3d19dab14440d7
khaled <kzack-dev@hotmail.com>
2020-08-21 03:43:04 +0300

    put everything in one file

--- src/utils.ts
+++
@@ -1,8 +0,0 @@
-export function isPlainObject(maybeObject: unknown) {
-  return (
-    typeof maybeObject == 'object' &&
-    maybeObject != null &&
-    maybeObject.constructor == Object &&
-    Object.prototype.toString.call(maybeObject) == '[object Object]'
-  );
-}
